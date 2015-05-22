c2b7d62e14df9a38459591e07ec14c0d0d8fd3df
xhmikosr <xhmikosr@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2015-05-22 16:02:21 +0000

    js/feed.js: lint tweaks.

--- misc/website/websitev2/js/feed.js
+++ misc/website/websitev2/js/feed.js
@@ -1,3 +1,5 @@
+/* globals moment, google */
+
 function summary(text, limit) {
     text = text.replace("/p/processhacker/code/", "https://sourceforge.net/p/processhacker/code/");
     if (text.length > limit) {
@@ -8,83 +10,83 @@ function summary(text, limit) {
     return text.trim();
 }
 
-function DisplayNewsFeed(result) {
+function displayNewsFeed(result) {
     if (!result.error) {
-        var rssoutput = "";
-        var thefeeds = result.feed.entries;
-        for (var i = 0; i < thefeeds.length; i++) {
-            rssoutput += "<div>";
-            rssoutput += "<a href=\"" + thefeeds[i].link + "\" target=\"_blank\" \">" + summary(thefeeds[i].title, 32) + "</a>";
-            rssoutput += "<span class=\"forumdate\"> by <span class=\"author\">" + thefeeds[i].author + "</span></span>";
-            rssoutput += "<div class=\"forumdate\">" + moment(thefeeds[i].publishedDate).fromNow() + " - " + new Date(thefeeds[i].publishedDate).toLocaleString() + "</div>";
-            rssoutput += "</div>";
+        var rssOutput = "";
+        var theFeeds = result.feed.entries;
+        for (var i = 0; i < theFeeds.length; i++) {
+            rssOutput += "<div>";
+            rssOutput += "<a href=\"" + theFeeds[i].link + "\" target=\"_blank\" \">" + summary(theFeeds[i].title, 32) + "</a>";
+            rssOutput += "<span class=\"forumdate\"> by <span class=\"author\">" + theFeeds[i].author + "</span></span>";
+            rssOutput += "<div class=\"forumdate\">" + moment(theFeeds[i].publishedDate).fromNow() + " - " + new Date(theFeeds[i].publishedDate).toLocaleString() + "</div>";
+            rssOutput += "</div>";
         }
-        window.newsfeedcontainer.innerHTML = rssoutput;
+        window.newsFeedContainer.innerHTML = rssOutput;
     } else {
-        window.newsfeedcontainer.innerHTML = "Error fetching feed: " + result.error.message;
+        window.newsFeedContainer.innerHTML = "Error fetching feed: " + result.error.message;
     }
 }
 
-function DisplayForumfeed(result) {
+function displayForumfeed(result) {
     if (!result.error) {
-        var rssoutput = "";
-        var thefeeds = result.feed.entries;
-        for (var i = 0; i < thefeeds.length; i++) {
-            rssoutput += "<div>";
-            rssoutput += "<a href=\"" + thefeeds[i].link + "\" target=\"_blank\" \">" + summary(thefeeds[i].title, 32) + "</a>";
-            rssoutput += "<span class=\"forumdate\"> by <span class=\"user\">" + thefeeds[i].author + "</span></span>";
-            rssoutput += "<div class=\"forumdate\">" + moment(thefeeds[i].publishedDate).fromNow() + " - " + new Date(thefeeds[i].publishedDate).toLocaleString() + "</div>";
-            rssoutput += "</div>";
+        var rssOutput = "";
+        var theFeeds = result.feed.entries;
+        for (var i = 0; i < theFeeds.length; i++) {
+            rssOutput += "<div>";
+            rssOutput += "<a href=\"" + theFeeds[i].link + "\" target=\"_blank\" \">" + summary(theFeeds[i].title, 32) + "</a>";
+            rssOutput += "<span class=\"forumdate\"> by <span class=\"user\">" + theFeeds[i].author + "</span></span>";
+            rssOutput += "<div class=\"forumdate\">" + moment(theFeeds[i].publishedDate).fromNow() + " - " + new Date(theFeeds[i].publishedDate).toLocaleString() + "</div>";
+            rssOutput += "</div>";
         }
-        window.forumfeedcontainer.innerHTML = rssoutput;
+        window.forumFeedContainer.innerHTML = rssOutput;
     } else {
-        window.newsfeedcontainer.innerHTML = "Error fetching feed: " + result.error.message;
+        window.newsFeedContainer.innerHTML = "Error fetching feed: " + result.error.message;
     }
 }
 
-function DisplaySvnFeed(result) 
-{
+function displaySvnFeed(result) {
     if (!result.error) {
-        var rssoutput = "";
-        var thefeeds = result.feed.entries;
-        for (var i = 0; i < thefeeds.length; i++) {
-            rssoutput += "<div>";
-            rssoutput += "<a href=\"" + thefeeds[i].link + "\" target=\"_blank\" \">" + summary(thefeeds[i].title, 32) + "</a>";
-            rssoutput += "<span class=\"forumdate\"> by <span style=\"color:#AA0000\">" + thefeeds[i].author + "</span></span>";
-            rssoutput += "<div class=\"forumdate\">" + moment(thefeeds[i].publishedDate).fromNow() + " - " + new Date(thefeeds[i].publishedDate).toLocaleString() + "</div>";
-            rssoutput += "</div>";
+        var rssOutput = "";
+        var theFeeds = result.feed.entries;
+        for (var i = 0; i < theFeeds.length; i++) {
+            rssOutput += "<div>";
+            rssOutput += "<a href=\"" + theFeeds[i].link + "\" target=\"_blank\" \">" + summary(theFeeds[i].title, 32) + "</a>";
+            rssOutput += "<span class=\"forumdate\"> by <span style=\"color:#AA0000\">" + theFeeds[i].author + "</span></span>";
+            rssOutput += "<div class=\"forumdate\">" + moment(theFeeds[i].publishedDate).fromNow() + " - " + new Date(theFeeds[i].publishedDate).toLocaleString() + "</div>";
+            rssOutput += "</div>";
         }
-        window.sourcefeedcontainer.innerHTML = rssoutput;
+        window.sourceFeedContainer.innerHTML = rssOutput;
     } else {
-        window.newsfeedcontainer.innerHTML = "Error fetching feed: " + result.error.message;
+        window.newsFeedContainer.innerHTML = "Error fetching feed: " + result.error.message;
     }
 }
 
 window.onload = function() {
-    window.newsfeedcontainer = document.getElementById("news_feed_div");
-    window.forumfeedcontainer = document.getElementById("forum_feed_div");    
-    window.sourcefeedcontainer = document.getElementById("source_feed_div");    
-    
+    window.newsFeedContainer = document.getElementById("news_feed_div");
+    window.forumFeedContainer = document.getElementById("forum_feed_div");
+    window.sourceFeedContainer = document.getElementById("source_feed_div");
+
     if (!window.google) {
-        window.newsfeedcontainer.innerHTML = "<div>Google API error.</div>";
-        window.forumfeedcontainer.innerHTML = "<div>Google API error.</div>";
-        window.sourcefeedcontainer.innerHTML = "<div>Google API error.</div>";
+        window.newsFeedContainer.innerHTML = "<div>Google API error.</div>";
+        window.forumFeedContainer.innerHTML = "<div>Google API error.</div>";
+        window.sourceFeedContainer.innerHTML = "<div>Google API error.</div>";
         return;
-    } else { 
-        window.newsfeedcontainer.innerHTML = "<div>Loading Latest News...</div>";
-        window.forumfeedcontainer.innerHTML = "<div>Loading Forum Activity...</div>";
-        window.sourcefeedcontainer.innerHTML = "<div>Loading Commit History...</div>";
-        
-        var newsfeedpointer = new google.feeds.Feed("http://wj32.org/processhacker/forums/feed.php?f=1");
-        newsfeedpointer.setNumEntries(5);
-        newsfeedpointer.load(DisplayNewsFeed);
-
-        var forumfeedpointer = new google.feeds.Feed("http://wj32.org/processhacker/forums/feed.php?mode=topics_active");
-        forumfeedpointer.setNumEntries(5);
-        forumfeedpointer.load(DisplayForumfeed);
-
-        var sourcefeedpointer = new google.feeds.Feed("http://sourceforge.net/p/processhacker/code/feed");
-        sourcefeedpointer.setNumEntries(5);
-        sourcefeedpointer.load(DisplaySvnFeed);
     }
+
+    window.newsFeedContainer.innerHTML = "<div>Loading Latest News...</div>";
+    window.forumFeedContainer.innerHTML = "<div>Loading Forum Activity...</div>";
+    window.sourceFeedContainer.innerHTML = "<div>Loading Commit History...</div>";
+
+    var newsfeedpointer = new google.feeds.Feed("http://wj32.org/processhacker/forums/feed.php?f=1");
+    newsfeedpointer.setNumEntries(5);
+    newsfeedpointer.load(displayNewsFeed);
+
+    var forumfeedpointer = new google.feeds.Feed("http://wj32.org/processhacker/forums/feed.php?mode=topics_active");
+    forumfeedpointer.setNumEntries(5);
+    forumfeedpointer.load(displayForumfeed);
+
+    var sourcefeedpointer = new google.feeds.Feed("http://sourceforge.net/p/processhacker/code/feed");
+    sourcefeedpointer.setNumEntries(5);
+    sourcefeedpointer.load(displaySvnFeed);
+
 };
